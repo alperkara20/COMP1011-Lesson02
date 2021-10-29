a70babfe7b2f76edd565fc0318c10ac404c1bbd7
@alperkara20 <alperkara247@gmail.com>
2021-10-29 21:13:53 +0300

    Updated the Hero class

--- COMP1011-Lesson02/src/Hero.java
+++ COMP1011-Lesson02/src/Hero.java
@@ -22,26 +22,31 @@ public class Hero {
 
 	//Constructor ------------------------
 	public Hero(String name) {
-		this.name=name;
+		this.name = name;
 		generateAbilities();
 	}
 
 	//Methods ----------------------------
 	void showAbilities() {
+		System.out.println("+++++++++++++++++++++++++");
 		System.out.println("Strength: " + this.strenght);
 		System.out.println("Speed: " + this.speed);
 		System.out.println("Health: " + this.health);
-
+		System.out.println("+++++++++++++++++++++++++");
 	}
     public void generateAbilities() {
+		this.strenght = (int)(Math.random() * 100 + 1);
+		this.speed = (int)(Math.random() * 100 + 1);
+		this.health = (int)(Math.random() * 100 + 1);
 		
 	}
 	public void fight() {
-		
+		System.out.println(this.name + " is fighting");
 	}
 	
 	public void run() {
-		
+		System.out.println(this.name + " is running");
+
 	}
 	
 	public void showName() {
